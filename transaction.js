77a9636910321c6a5da0df7610410d2c1c756024
m.t-protain <rihitosakuraa155@gmail.com>
2020-04-12 21:26:37 +0900

    トランザクション処理、beginとcommit

---
+++ transaction.js
@@ -0,0 +1,33 @@
+// dbの特定
+const { Client } = require("pg");
+const client = new Client({
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: "5432",
+  database: "mydb",
+});
+
+async function transaction() {
+  try {
+    await client.connect();
+    // BEGIN
+    await client.query("BEGIN");
+    await client.query("select * from staff");
+    await client.query("insert into staff values ($1, $2, $3)", [
+      2,
+      "hanako",
+      18,
+    ]);
+    console.log("insert a new row");
+    // COMMIT
+    await client.query("COMMIT");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+    console.log("Client disconnected successfully");
+  }
+}
+
+transaction();
